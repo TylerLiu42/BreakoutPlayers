b3724fa24cdb0db1ccda107937eb7a42b83e44c8
Tyler Liu <tgqliu@uwaterloo.ca>
2020-05-04 18:18:29 -0400

    Add separate expected hitting table

--- schema.sql
+++ schema.sql
@@ -1,17 +1,42 @@
 CREATE TABLE StatcastData (
    lastName varchar(30),
    firstName varchar(30), 
+   playerID int,
+   attempts int,
+   avgHitAngle double,
+   sweetSpotPercentage double,
+   maxHitSpeed double,
+   avgHitSpeed double,
+   fbld double,
+   gb double,
+   maxDistance int,
+   avgDistance int,
+   avgHRDistance int default 0,
+   ev95plus int,
+   ev95percent double,
+   barrels int,
+   barrelPercentage double,
+   barrelPerPA double,
+
+   PRIMARY KEY (playerID)
+);
+
+CREATE TABLE StatcastDataExpectedHitting (
+   lastName varchar(30),
+   firstName varchar(30),
+   playerID int,
    year int,
-   xba double,
+   pa int,
+   bip int,
+   ba double,
+   xBA double,
+   xBADiff double,
+   slg double,
    xslg double,
+   xslgDiff double,
    woba double,
    xwoba double,
-   xobp double,
-   xiso double,
-   exitVelo double,
-   launchAngle double,
-   sweetSpotPercentage double,
-   barrelRate double,
+   xwobaDiff double,
 
-   PRIMARY KEY (lastName, firstName)
+   PRIMARY KEY (playerID)
 );
