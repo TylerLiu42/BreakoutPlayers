4d59b37190bf8b51076adedab08355bcff80770a
Tyler Liu <tgqliu@uwaterloo.ca>
2020-05-04 18:22:15 -0400

    Get csv data via http

--- src/dbConnection.py
+++ src/dbConnection.py
@@ -5,8 +5,8 @@ Created on May 2, 2020
 '''
 import mysql.connector
 import csv
-from percentileQueries import findLowWobaPlayers, findHighExitVeloPlayers, findHighBarrelPlayers
-import pandas as pd
+from percentileQueries import findLowWobaPlayers, findHighExitVeloPlayers, findHighBarrelPlayers,\
+    statcast_batter_exitVeloBarrels, statcast_batter_expectedHitting
 
 pwFile = open("pw.txt", "r")
 password = pwFile.readline()
@@ -23,27 +23,46 @@ mydb = mysql.connector.connect(
 
 cursor = mydb.cursor(prepared=True)
 
+res = statcast_batter_exitVeloBarrels(2019, 100)
+with open("statcast2019.csv", "wb") as fp:
+    fp.write(res)
+    
+res = statcast_batter_expectedHitting(2019, 100)
+with open("statcastExpectedHitting.csv", "wb") as fp:
+    fp.write(res)
+
 csv_data = csv.reader(open('statcast2019.csv'))
 next(csv_data)
+totalRowsLaunchData = 0;
+totalRowsExpectedHitting = 0;
 for row in csv_data:
-    cursor.execute('INSERT INTO statcastData(lastName, firstName, year, \
-          xba, xslg, woba, xwoba, xobp, xiso, exitVelo, launchAngle, \
-          sweetSpotPercentage, barrelRate )' \
+    totalRowsLaunchData += 1 
+    if (row[12] == ''): row[12] = 0
+    cursor.execute('INSERT INTO statcastData(lastName, firstName, playerID, \
+          attempts, avgHitAngle, sweetSpotPercentage, maxHitSpeed, avgHitSpeed, fbld, gb, maxDistance, \
+          avgDistance, avgHRDistance, ev95plus, ev95percent, barrels, barrelPercentage, barrelPerPA)' \
           'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, \
-          %s, %s, %s, %s)', 
+          %s, %s, %s, %s, %s, %s, %s, %s, %s)', 
           row)
-cursor.execute("select count(*) from statcastData")
-res = cursor.fetchone()
-totalRows = res[0]
 
-lowWobaPlayers = findLowWobaPlayers(mydb, totalRows)
-highExitVeloPlayers = findHighExitVeloPlayers(mydb, totalRows)
-highBarrelPlayers = findHighBarrelPlayers(mydb, totalRows)
+csv_data = csv.reader(open('statcastExpectedHitting.csv'))
+next(csv_data)
+for row in csv_data:
+    totalRowsExpectedHitting += 1
+    cursor.execute("""
+        INSERT INTO StatcastDataExpectedHitting(lastName, firstName, playerID,
+        year, pa, bip, ba, xba, xbaDiff, slg, xslg, xslgDiff, woba, xwoba, xwobaDiff)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, row)
+
+lowWobaPlayers = findLowWobaPlayers(mydb, totalRowsExpectedHitting)
+highExitVeloPlayers = findHighExitVeloPlayers(mydb, totalRowsLaunchData)
+highBarrelPlayers = findHighBarrelPlayers(mydb, totalRowsLaunchData)
 breakoutPlayers = list(set(highExitVeloPlayers) & set(highBarrelPlayers) & set(lowWobaPlayers))
 for x in breakoutPlayers:
     print(x)
     outputFile.write(x[0] + "," + x[1] + "\n")
-    
+
 pwFile.close()
 outputFile.close()
     
--- src/percentileQueries.py
+++ src/percentileQueries.py
@@ -4,31 +4,47 @@ Created on May 2, 2020
 @author: Tyler Liu
 '''
 import math
+import requests
+import pandas as pd
+import io
 
 cutOffWobaPercentile = 0.5
 cutOffExitVeloPercentile = 0.8
 cutOffBarrelPercentile = 0.7
 
-def findLowWobaPlayers(mydb, totalRows):
+def findLowWobaPlayers(mydb, totalRowsLaunchData):
     cursor = mydb.cursor()
-    row = cutOffWobaPercentile*totalRows
+    row = cutOffWobaPercentile*totalRowsLaunchData
     cursor.execute("""select lastName, firstName from 
-            statCastData order by woba asc limit %s""", [math.ceil(row)])
+            StatcastDataExpectedHitting 
+            order by woba asc limit %s""", [math.ceil(row)])
     resultSet = cursor.fetchall()
     return resultSet
 
-def findHighExitVeloPlayers(mydb, totalRows):
+def findHighExitVeloPlayers(mydb, totalRowsLaunchData):
     cursor = mydb.cursor()
-    row = cutOffExitVeloPercentile*totalRows
+    row = cutOffExitVeloPercentile*totalRowsLaunchData
     cursor.execute("""select lastName, firstName from statCastData 
-            order by exitVelo desc limit %s""", [totalRows-math.ceil(row)])
+            order by avgHitSpeed desc limit %s""", [totalRowsLaunchData-math.ceil(row)])
     resultSet = cursor.fetchall()
     return resultSet
 
-def findHighBarrelPlayers(mydb, totalRows):
+def findHighBarrelPlayers(mydb, totalRowsLaunchData):
     cursor = mydb.cursor()
-    row = cutOffBarrelPercentile*totalRows
+    row = cutOffBarrelPercentile*totalRowsLaunchData
     cursor.execute("""select lastName, firstName from statCastData
-            order by barrelRate desc limit %s""", [totalRows-math.ceil(row)])
+            order by barrelPercentage desc limit %s""", [totalRowsLaunchData-math.ceil(row)])
     resultSet = cursor.fetchall()
     return resultSet
+
+def statcast_batter_exitVeloBarrels(year, minBBE="q"):
+    url = f"https://baseballsavant.mlb.com/leaderboard/statcast?type=batter&year={year}&position=&team=&min={minBBE}&csv=true"
+    res = requests.get(url, timeout=None).content
+    data = pd.read_csv(io.StringIO(res.decode('utf-8')))
+    print(data)
+    return res
+
+def statcast_batter_expectedHitting(year, minBBE="q"):
+    url = f"https://baseballsavant.mlb.com/leaderboard/expected_statistics?type=batter&year={year}&position=&team=&min={minBBE}&csv=true"
+    res = requests.get(url, timeout=None).content
+    return res;
